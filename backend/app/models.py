e19d929ac343ffbaa6ac3dc1e859cb4a40dad17a
Aytsuu <paoloaraneta008@gmail.com>
2025-05-28 15:54:46 +0800

    Update

--- backend/app/models.py
+++ backend/app/models.py
@@ -14,11 +14,25 @@ class Product(models.Model):
   prod_quantity = models.IntegerField()
   prod_image = models.TextField()
 
+  class Meta:
+    db_table = 'product'
+
 class Order(models.Model):
   ord_id = models.BigAutoField(primary_key=True)
   ord_date = models.DateField()
   ord_status = models.CharField(max_length=20)
   prod = models.ForeignKey(Product, on_delete=models.CASCADE)
   acc = models.ForeignKey(Account, on_delete=models.CASCADE)
-  
+
+  class Meta:
+    db_table = 'order'
+
+class Transaction(models.Model):
+  trans_id = models.BigAutoField(primary_key=True)
+  trans_amount = models.FloatField()
+  prod = models.ForeignKey(Product, on_delete=models.CASCADE)
+  acc = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = 'transaction'
   
